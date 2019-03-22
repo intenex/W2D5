85c148d9ecb0d33c0b56784191884466cfd9611c
Nick <intenex@gmail.com>
2019-03-22 12:24:48 -0700

    finishes a bunch of hash map specs

--- hash_map/lib/p05_hash_map.rb
+++ hash_map/lib/p05_hash_map.rb
@@ -1,6 +1,8 @@
 require_relative 'p04_linked_list'
+require "byebug"
 
 class HashMap
+  include Enumerable
   attr_accessor :count
 
   def initialize(num_buckets = 8)
@@ -9,27 +11,44 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    linked_list = bucket(key)
+    if linked_list.include?(key)
+      linked_list.update(key, val)
+    else
+      linked_list.append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    linked_list = bucket(key)
+    if linked_list.include?(key)
+      linked_list.remove(key)
+      @count -= 1
+    end
   end
 
   def each
+    @store.each do |bucket|
+      bucket.each {|node| yield(node.key, node.val) }
+    end
   end
 
   # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -45,5 +64,6 @@ class HashMap
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % @store.length]
   end
 end
