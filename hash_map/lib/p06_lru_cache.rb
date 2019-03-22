27b6c96caca479af34a1706d89f9d8088a0e91e9
Nick <intenex@gmail.com>
2019-03-22 14:36:11 -0700

    Finishes LRU cache

--- hash_map/lib/p06_lru_cache.rb
+++ hash_map/lib/p06_lru_cache.rb
@@ -1,5 +1,6 @@
 require_relative 'p05_hash_map'
 require_relative 'p04_linked_list'
+require "byebug"
 
 class LRUCache
   def initialize(max, prc)
@@ -14,6 +15,17 @@ class LRUCache
   end
 
   def get(key)
+    if @map.include?(key)
+      node = @map[key]
+      update_node!(node)
+      node.val
+    else
+      value = @prc.call(key)
+      @store.append(key, value)
+      @map[key] = @store.last
+      eject! if @map.count > @max
+      value
+    end
   end
 
   def to_s
@@ -28,8 +40,15 @@ class LRUCache
 
   def update_node!(node)
     # suggested helper method; move a node to the end of the list
+    @store.remove(node.key)
+    @store.append(node.key, node.val)
+    @map[node.key] = @store.last
   end
 
   def eject!
+    # debugger
+    hash_key = @store.first.key
+    @store.remove(hash_key)
+    @map.delete(hash_key)
   end
 end
